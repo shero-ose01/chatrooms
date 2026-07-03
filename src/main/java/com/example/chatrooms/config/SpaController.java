5b4523316fc230a97e45a816a11e403106a3e26a
Shero Ose <s.ose22601@gmail.com>
2026-07-03 18:05:35 +0200

    changes didnt get pushed somehow

---
+++ src/main/java/com/example/chatrooms/config/SpaController.java
@@ -0,0 +1,12 @@
+package com.example.chatrooms.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpaController {
+    @RequestMapping(value = {"/", "/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
+    public String forward(){
+        return "forward:/index.html";
+    }
+}
